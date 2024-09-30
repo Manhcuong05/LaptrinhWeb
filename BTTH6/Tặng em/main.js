67b82c0474a0dd7868b43b822aba32475292cb0d
Manhcuong05 <ockun68@gmail.com>
2024-09-30 20:05:04 +0700

    c

---
+++ BTTH6/Tặng em/main.js
@@ -0,0 +1,5 @@
+// Lắng nghe sự kiện bấm vào nút và chuyển hướng đến trang chúc mừng
+document.getElementById('click-button').addEventListener('click', function() {
+    // Chuyển đến trang lời chúc
+    window.location.href = 'chucmung.html';
+});
