90c1253de19eda8080c9488a874d3eac540b27dc
Cường Nguyễn <149052047+Manhcuong05@users.noreply.github.com>
2024-09-30 20:08:37 +0700

    Delete BTTH6/Tặng em directory

--- BTTH6/Tặng em/main.js
+++
@@ -1,5 +0,0 @@
-// Lắng nghe sự kiện bấm vào nút và chuyển hướng đến trang chúc mừng
-document.getElementById('click-button').addEventListener('click', function() {
-    // Chuyển đến trang lời chúc
-    window.location.href = 'chucmung.html';
-});
